3458f3c668beb404274e871e318a222337ea4743
unknown <Administrator@SSSLBG4NB712.SONATA.LOCAL>
2022-11-21 10:45:38 +0530

    Initial Version

---
+++ src/test/java/com/example/demo/SonatatrainingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.impl.DefaultAddition;
+
+@SpringBootApplication
+class SonatatrainingApplicationTests {
+
+
+public static void main(String []args) {
+		SpringApplication.run(SonatatrainingApplicationTests.class, args);
+		AddInterface addition = new DefaultAddition();
+		System.out.println(addition.add(10, 20));
+	}
+
+}
